62439485e5053d9a970b1a4306fcc3f33db54e30
Svyatoslav Kushnarev <87011534+svtsv01@users.noreply.github.com>
2025-07-28 11:28:19 +1000

    change

--- my-react-app/src/api/authService.js
+++ my-react-app/src/api/authService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = `https://dummyjson.com`;
 
 export const loginUser = async (username, password) => {
   const response = await fetch(`${API_BASE_URL}/auth/login`, {
