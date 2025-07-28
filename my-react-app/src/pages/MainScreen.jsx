f986c6ccb30fef2f682ead795b1efc1cdf680276
Svyatoslav Kushnarev <87011534+svtsv01@users.noreply.github.com>
2025-07-28 11:33:35 +1000

    Fixed

--- my-react-app/src/pages/MainScreen.jsx
+++ my-react-app/src/pages/MainScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import TodoList from '../components/ToDo/ToDoList/TodoList';
 
 const MainScreen = () => {
@@ -9,6 +10,7 @@ const MainScreen = () => {
       <main className='app-content'>
         <TodoList />
       </main>
+      <Footer/>
     </div>
   );
 };
